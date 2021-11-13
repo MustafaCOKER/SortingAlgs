4018595f63965cf463b52a7155ea54a736ee39a5
MustafaCOKER <mustafacoker@yandex.com>
2021-11-13 21:44:05 +0300

    bug fix

--- include/Tools.h
+++ include/Tools.h
@@ -7,46 +7,51 @@ typedef unsigned char boolean;
 
 typedef void (*t_sswap)(void *, size_t, size_t);
 typedef boolean (*t_greaterThan)(void *, size_t, size_t);
+typedef boolean (*t_lessThan)(void *, size_t, size_t);
 typedef boolean (*t_compareNthItemsOf2Array)(const void *, const void *, size_t);
 
 // Macro Definitions
 
-#define TRUE    1
-#define FALSE   0
+#define TRUE 1
+#define FALSE 0
 
-#define GOTO_INDEX(ttype, startPos, index)                                  \
-    ( * ((ttype*) (startPos + index * sizeof(ttype))) )
+#define GOTO_INDEX(ttype, startPos, index) \
+    (*((ttype *)(startPos + index * sizeof(ttype))))
 
-#define DEF_GREATER_THAN( type, fooName )                                   \
-    boolean fooName(void* p1, size_t index1, size_t index2)                 \
+#define DEF_GREATER_THAN(type, fooName)                                     \
+    boolean fooName(void *p1, size_t index1, size_t index2)                 \
     {                                                                       \
-        return GOTO_INDEX( type, p1, index1 )                               \
-             > GOTO_INDEX( type, p1, index2 );                              \
+        return GOTO_INDEX(type, p1, index1) > GOTO_INDEX(type, p1, index2); \
     }
 
-#define DEF_SWAP( type, fooName )                                           \
-    void fooName(void* p1, size_t index1, size_t index2)                    \
+#define DEF_LESS_THAN(type, fooName)                                        \
+    boolean fooName(void *p1, size_t index1, size_t index2)                 \
     {                                                                       \
-        type temp = GOTO_INDEX( type, p1, index2 );                         \
-        GOTO_INDEX( type, p1, index2 ) = GOTO_INDEX( type, p1, index1 );    \
-        GOTO_INDEX( type, p1, index1 ) = temp;                              \
+        return GOTO_INDEX(type, p1, index1) < GOTO_INDEX(type, p1, index2); \
     }
 
-#define DEF_PRINT( type, fooName, printfFrmt)                               \
-    void fooName(void* p, size_t index)                                     \
-    {                                                                       \
-        printf(printfFrmt, GOTO_INDEX( type, p, index ) );                  \
+#define DEF_SWAP(type, fooName)                                      \
+    void fooName(void *p1, size_t index1, size_t index2)             \
+    {                                                                \
+        type temp = GOTO_INDEX(type, p1, index2);                    \
+        GOTO_INDEX(type, p1, index2) = GOTO_INDEX(type, p1, index1); \
+        GOTO_INDEX(type, p1, index1) = temp;                         \
     }
 
-#define DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(type, fooName )                     \
-    boolean fooName(const void * arr1, const void * arr2, size_t index)     \
-    {                                                                       \
-        if (    GOTO_INDEX( type, arr1, index ) ==                          \
-                GOTO_INDEX( type, arr2, index ) )                           \
-            return TRUE;                                                    \
-        return FALSE;                                                       \
+#define DEF_PRINT(type, fooName, printfFrmt)            \
+    void fooName(void *p, size_t index)                 \
+    {                                                   \
+        printf(printfFrmt, GOTO_INDEX(type, p, index)); \
     }
 
+#define DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(type, fooName)                \
+    boolean fooName(const void *arr1, const void *arr2, size_t index) \
+    {                                                                 \
+        if (GOTO_INDEX(type, arr1, index) ==                          \
+            GOTO_INDEX(type, arr2, index))                            \
+            return TRUE;                                              \
+        return FALSE;                                                 \
+    }
 
 // Function Declarations.
 
--- test/include/TestTools.hpp
+++ test/include/TestTools.hpp
@@ -11,43 +11,47 @@ const char varArrChar_sorted[] = "   !MPSaeeelorst";
 const int varArrInt_sorted[] = {0, 1, 2, 4, 6, 8, 9};
 
 #define TEST_ARR_SIZE 4096
-#define FILL(arr) std::memset(arr, 0, sizeof( arr ));
+#define FILL(arr) std::memset(arr, 0, sizeof(arr));
 
-int varArrIntD[ TEST_ARR_SIZE ];
-int varArrIntDSorted[ TEST_ARR_SIZE ];
+int varArrIntD[TEST_ARR_SIZE];
+int varArrIntDSorted[TEST_ARR_SIZE];
 
-char varArrCharD[ TEST_ARR_SIZE ];
-char varArrCharDSorted[ TEST_ARR_SIZE ];
+char varArrCharD[TEST_ARR_SIZE];
+char varArrCharDSorted[TEST_ARR_SIZE];
 
-double varArrDoubleD[ TEST_ARR_SIZE ];
-double varArrDoubleDSorted[ TEST_ARR_SIZE ];
+double varArrDoubleD[TEST_ARR_SIZE];
+double varArrDoubleDSorted[TEST_ARR_SIZE];
 
-double varArrFloatD[ TEST_ARR_SIZE ];
-double varArrFloatDSorted[ TEST_ARR_SIZE ];
+double varArrFloatD[TEST_ARR_SIZE];
+double varArrFloatDSorted[TEST_ARR_SIZE];
 
 // definitions of required functions for int
 DEF_GREATER_THAN(int, greaterThan_int);
+DEF_LESS_THAN(int, lessThan_int);
 DEF_SWAP(int, swap_int);
 DEF_PRINT(int, print_int, "%d ");
 DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(int, compareItems_int);
 
 // definitions of required functions for char
 DEF_GREATER_THAN(char, greaterThan_char);
+DEF_LESS_THAN(char, lessThan_char);
 DEF_SWAP(char, swap_char);
 DEF_PRINT(char, print_char, "%c ");
 DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(char, compareItems_char);
 
 // definitions of required functions for double
-DEF_GREATER_THAN(char, greaterThan_double);
-DEF_SWAP(char, swap_double);
-DEF_PRINT(char, print_double, "%c ");
-DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(char, compareItems_double);
+DEF_GREATER_THAN(double, greaterThan_double);
+DEF_LESS_THAN(double, lessThan_double);
+DEF_SWAP(double, swap_double);
+DEF_PRINT(double, print_double, "%c ");
+DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(double, compareItems_double);
 
 // definitions of required functions for float
-DEF_GREATER_THAN(char, greaterThan_float);
-DEF_SWAP(char, swap_float);
-DEF_PRINT(char, print_float, "%c ");
-DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(char, compareItems_float);
+DEF_GREATER_THAN(float, greaterThan_float);
+DEF_LESS_THAN(float, lessThan_float);
+DEF_SWAP(float, swap_float);
+DEF_PRINT(float, print_float, "%c ");
+DEF_COMPARE_Nth_ITEMs_Of_2ARRAY(float, compareItems_float);
 
 void initTestArr()
 {
@@ -69,24 +73,23 @@ void generateTestArr()
     std::srand(std::time(nullptr));
     int randomNum = 0;
 
-    for (size_t i=0; i<TEST_ARR_SIZE; ++i)
+    for (size_t i = 0; i < TEST_ARR_SIZE; ++i)
     {
         randomNum = std::rand() % TEST_ARR_SIZE;
 
         varArrIntD[i] = randomNum;
-        varArrCharD[i] = (char) 32 + randomNum % 95;
-        varArrDoubleD[i] = (double) randomNum;
-        varArrFloatD[i] = (float) randomNum;
+        varArrCharD[i] = (char)32 + randomNum % 95;
+        varArrDoubleD[i] = (double)randomNum;
+        varArrFloatD[i] = (float)randomNum;
 
         varArrIntDSorted[i] = varArrIntD[i];
         varArrCharDSorted[i] = varArrCharD[i];
         varArrDoubleDSorted[i] = varArrDoubleD[i];
         varArrFloatDSorted[i] = varArrFloatD[i];
     }
-    
+
     bubbleSort(varArrIntDSorted, TEST_ARR_SIZE, greaterThan_int, swap_int);
     bubbleSort(varArrCharDSorted, TEST_ARR_SIZE, greaterThan_char, swap_char);
     bubbleSort(varArrDoubleDSorted, TEST_ARR_SIZE, greaterThan_double, swap_double);
     bubbleSort(varArrFloatDSorted, TEST_ARR_SIZE, greaterThan_float, swap_float);
-
 }
